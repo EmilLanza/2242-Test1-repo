a19ef3e69a9fc50519b30ea2fe5cabeee5fd7fd6
Emil F. Lanza <2019120200@gmail.com>
2023-03-31 08:47:03 -0600

    All Functions created

--- main.go
+++ main.go
@@ -2,8 +2,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+	"math/rand"
 )
 
 
@@ -12,7 +15,7 @@ func main(){
 	mux := http.NewServeMux() //create multiplexer
 	mux.HandleFunc("/", home) //pass key "/" and value home
 	mux.HandleFunc("/greeting", greeting) //pass key "/greeting" and value greeting
-	//mux.HandleFunc("/random", random) //pass key "/random" and value random
+	mux.HandleFunc("/random", random) //pass key "/random" and value random
 
 	log.Print("starting server on: 4000") //print line
 	err := http.ListenAndServe(": 4000", mux) //create server
@@ -26,6 +29,35 @@ func home(w http.ResponseWriter, r *http.Request){ //deals with http methods
 }
 
 func greeting(w http.ResponseWriter, r *http.Request){
+	/*Now := time.Now()
+	hr, min := Now.Clock()
+	day := Now.Day()
+	fmt.Println(day, hr,":",min)
+	*/
 	Now := time.Now()
+    day := Now.Weekday().String()
+    hour := Now.Hour()
+
+	var greeting string
+    if hour >= 5 && hour < 12 {
+        greeting = "Good morning!"
+    } else if hour >= 12 && hour < 18 {
+        greeting = "Good afternoon!"
+    } else {
+        greeting = "Good night!"
+    }
+
+	html := fmt.Sprintf("<html><body><h1>%s</h1><p>Today is %s.</p><p>The time is %s.</p></body></html>", greeting, day, Now.Format("3:04"))
+    fmt.Fprintf(w, html)
+
 }
 
+func random(w http.ResponseWriter, r *http.Request){
+	qoute := []string{ //create array
+		"To be or not to be",
+		"Hello from mars",
+		"Not random enough",
+		"Just Hello",
+	}
+	w.Write([]byte(qoute[rand.Intn(len(qoute))]))
+}
